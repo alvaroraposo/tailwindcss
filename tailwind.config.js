70e36ebea04ee626660ece60857a5fb213da0ee5
alvaroraposo <alvaroraposo@yahoo.com.br>
2021-11-09 18:01:21 -0400

    'tailwind.config.js'

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  //npx tailwindcss init generates this file
+  /* Removes unused styles from the production files */
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  /* customizes tailwind utility classes */
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
