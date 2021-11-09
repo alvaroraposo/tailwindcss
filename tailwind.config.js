dfa5710d5bcd7f564068cae816f9b98af0b33c61
alvaroraposo <alvaroraposo@yahoo.com.br>
2021-11-09 18:51:37 -0400

    JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   // npm install postcss autoprefixer postcss-cli
   // npx tailwindcss init -p
   /* Removes unused styles from the production files */
-  purge: [],
+  mode: "jit",
+  purge: ["./public/**/*.{html,js}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   /* customizes tailwind utility classes */
   theme: {
