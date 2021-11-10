b287abe91adcd65b0b373041aee0584b189a7035
alvaroraposo <alvaroraposo@yahoo.com.br>
2021-11-09 21:02:35 -0400

    theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   /* customizes tailwind utility classes */
   theme: {
+    screens: {
+      sm: "600px",
+      md: "700px",
+      lg: "900px",
+      xl: "1400px",
+    },
+    colors: {
+      indigo: {
+        light: "#b3bcf5",
+        DEFAULT: "#5c6ac4",
+        dark: "#202e78",
+      },
+    },
+    spacing: {
+      1: "0.35rem",
+      2: "0.75rem",
+    },
     extend: {},
   },
   variants: {
